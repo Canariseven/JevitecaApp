33f3aff6184c5d5e4a139ec3377b6187382710e0
Carmelo Ruymán Quintana Santana <canariseven@gmail.com>
2015-06-25 21:04:45 +0100

    Con los nuevos JSON , añadido las imágenes

--- scripts/directives/AlbumsDirective.js
+++ scripts/directives/AlbumsDirective.js
@@ -11,31 +11,6 @@ angular.module("JevitecaApp")
                 albums: "="
             },
             link: function (scope, element) {
-                scope.imageStd = "resources/img/imgDefault.jpg";
-          /*      scope.$evalAsync(function(){
-                    var position = element;
-                    console.log(position);
-                })*/
-                /*scope.routeImage = function (id) {
-                    var diferido = $q.defer();
-                    $http.get("resources/data/albums.json").then(
-                        function (datos) {
-                            debugger;
-                            single_object = $filter('filter')(datos.data, function (d) {
-                                return d.id === id;
-                            });
-                            debugger;
-                            if (single_object.length == 1)
-                            diferido.resolve(single_object[0].image);
-                            else
-                            diferido.reject("");
-                        },
-                        function () {
-                            diferido.reject("");
-                        }
-                    );
-                    return diferido.promise
-                }*/
 
             }
         };
